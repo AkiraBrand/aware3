8a78baedcd5688e2c17944f01e0858bc659c169c
Ruth Brand <ruthbrandsoprano@gmail.com>
2020-07-17 16:57:08 -0700

    adds constructor

--- test/church-test.js
+++ test/church-test.js
@@ -2,9 +2,16 @@ var assert = require('chai').assert;
 var Church = require('../lib/church.js');
 
 describe('Church', function() {
-  it('has attributes', function() {
-    var church = new Church('Catholic');
+
+  it('exists', function() {
+    assert.isFunction(Church)
+  });
+
+  it('has attributes', function(){
+    var church = new Church('Catholic', 100)
 
     assert.equal(church.denomination, 'Catholic');
+    assert.equal(church.congregationSize, 100);
   });
+
 });
