b2318722cd7752548cf4fdc0f84f8f62d8a916ef
Ruth Brand <ruthbrandsoprano@gmail.com>
2020-07-17 17:12:13 -0700

    adds add congregant function

--- test/church-test.js
+++ test/church-test.js
@@ -8,12 +8,30 @@ describe('Church', function() {
   });
 
   it('has attributes', function(){
-    var church = new Church('Catholic', 100, true, ["guitar"]);
+    var church = new Church('Catholic', 100, true, "guitar");
 
     assert.equal(church.denomination, 'Catholic');
     assert.equal(church.congregationSize, 100);
     assert.equal(church.isOpen, true);
     assert.equal(church.worshipBand, "guitar");
   });
+
+  it('can close down', function() {
+    var church = new Church('Catholic', 100, true, "guitar");
+
+    church.closeDown();
+
+    assert.equal(church.isOpen, false);
+  });
+
+  it('can add congregants', function(){
+    var church = new Church('Catholic', 100, true, "guitar");
+
+    church.addCongregant();
+
+    assert.equal(church.congregationSize, 101);
+  });
+
+
 
 });
