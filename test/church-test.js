2fe8a4d4644a08c34e6f9ff051404db1431a4045
Ruth Brand <ruthbrandsoprano@gmail.com>
2020-07-17 19:12:20 -0700

    wip

--- test/church-test.js
+++ test/church-test.js
@@ -1,4 +1,5 @@
 var assert = require('chai').assert;
+var deepEql = require("deep-eql");
 var Church = require('../lib/church.js');
 
 describe('Church', function() {
@@ -8,30 +9,38 @@ describe('Church', function() {
   });
 
   it('has attributes', function(){
-    var church = new Church('Catholic', 100, true, "guitar");
+    var church = new Church('Catholic', 100, true);
 
     assert.equal(church.denomination, 'Catholic');
     assert.equal(church.congregationSize, 100);
     assert.equal(church.isOpen, true);
-    assert.equal(church.worshipBand, "guitar");
+    assert.equal(church.worshipBand, []);
   });
 
   it('can close down', function() {
-    var church = new Church('Catholic', 100, true, "guitar");
+    var church = new Church('Catholic', 100, true);
 
     church.closeDown();
 
     assert.equal(church.isOpen, false);
   });
 
-  it('can add congregants', function(){
-    var church = new Church('Catholic', 100, true, "guitar");
+  it('can add congregants', function() {
+    var church = new Church('Catholic', 100, true);
 
     church.addCongregant();
 
     assert.equal(church.congregationSize, 101);
   });
 
+  it('can add worship band instruments', function() {
+    var church = new Church('Catholic', 100, true);
+
+    church.addWorshipInstruments("piano");
+
+    expect({church.worshipBand})
+    assert.deepEqual((church.worshipBand), ["piano"]);
+  });
 
 
 });
